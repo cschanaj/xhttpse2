9223953ddc75c2229a8eba2a1e86d0de6698b0f7
Pasu Chan <cschanaj@users.noreply.github.com>
2017-02-20 17:32:26 +0800

    Create DifferentContent.h

---
+++ src/modules/DifferentContent.h
@@ -0,0 +1,9 @@
+#include "../HttpseXData.h"
+
+#ifndef HTTPSE_DIFFERENT_CONTENT_H
+#define HTTPSE_DIFFERENT_CONTENT_H
+
+HttpseCode
+httpse_check_different_content(const HttpseTData *tdata);
+
+#endif /* HTTPSE_DIFFERENT_CONTENT_H */
