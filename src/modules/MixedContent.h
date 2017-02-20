e90e5fd5d5ac479de2e44071569735729ba90d1a
Pasu Chan <cschanaj@users.noreply.github.com>
2017-02-20 17:31:51 +0800

    Create MixedContent.h

---
+++ src/modules/MixedContent.h
@@ -0,0 +1,9 @@
+#include "../HttpseXData.h"
+
+#ifndef HTTPSE_MIXED_CONTENT_H
+#define HTTPSE_MIXED_CONTENT_H
+
+HttpseCode
+httpse_check_mixed_content(const HttpseTData *tdata);
+
+#endif /* HTTPSE_MIXED_CONTENT_H */
