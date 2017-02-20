839e7cd4f630b430befdc01af3067285330eb870
Pasu Chan <cschanaj@users.noreply.github.com>
2017-02-20 17:31:14 +0800

    Create SecureFallback.h

---
+++ src/modules/SecureFallback.h
@@ -0,0 +1,9 @@
+#include "../HttpseXData.h"
+
+#ifndef HTTPSE_SECURE_FALLBACK_H
+#define HTTPSE_SECURE_FALLBACK_H
+
+HttpseCode
+httpse_check_secure_fallback(const HttpseTData *tdata);
+
+#endif /* HTTPSE_SECURE_FALLBACK_H */
