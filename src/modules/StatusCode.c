57830441ac4e0d9f470798859c7e41aae75b41b0
cschanaj <ccspasu@gmail.com>
2017-03-17 10:16:29 +0800

    Fix coding style and improve readability

--- src/modules/StatusCode.c
+++ src/modules/StatusCode.c
@@ -3,20 +3,22 @@
 HttpseCode
 httpse_check_status_code(const HttpseTData *tdata)
 {
-	long cp = 0, cs = 0;
+	long cp = 0;
+	long cs = 0;
+
 
 	curl_easy_getinfo(tdata->rp->curl, CURLINFO_RESPONSE_CODE, &cp);
 	curl_easy_getinfo(tdata->rs->curl, CURLINFO_RESPONSE_CODE, &cs);
 
-	if(cs && cp)
-	{
-		if(cs != cp)
-		{
-			return HTTPSE_STATUS_CODE_MISMATCH;
-		}
 
+	if(cs == cp)
+	{
 		switch(cp/ 100)
 		{
+		/* Remark: unable to recieve HTTP response code, skip tests */
+		case 0:
+
+		/* Remark: everything is OK */
 		case 2:
 		case 3:
 			return HTTPSE_OK;
@@ -31,7 +33,5 @@ httpse_check_status_code(const HttpseTData *tdata)
 			return HTTPSE_STATUS_CODE_OTHERS;
 		}
 	}
-
-	/* Remark: Unreachable code. */
-	return HTTPSE_ERROR_UNKNOWN;
+	return HTTPSE_STATUS_CODE_MISMATCH;
 }
