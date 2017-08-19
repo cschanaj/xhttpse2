57f3ee58bc9953dc247485020ad8f173e5d3ecec
Pasu Chan <ccspasu@gmail.com>
2017-08-19 21:52:08 +0800

    Add support for ruleset#name tag

--- script/ruleset-update.sh
+++ script/ruleset-update.sh
@@ -5,17 +5,20 @@ TEMP2=`mktemp`
 
 PATTERN1='^\s*- \([^\n]*\)$'
 PATTERN2='^\s*<target\s\+host\s*=\s*"\([^"]*\)"\s*\/>\s*\?$'
+PATTERN3='^[^<]*<ruleset\s\+name\s*=\s*"\([^"]*\)"[^>]*>\s*\?$'
 
 # Extract subdomains from existing rulesets
 sed -e "/$PATTERN1/!d" -e "s/$PATTERN1/\1/g" "$1" >> "$TEMP1"
 sed -e "/$PATTERN2/!d" -e "s/$PATTERN2/\1/g" "$1" >> "$TEMP1"
+
+NAME=`sed -e "/$PATTERN3/!d" -e "s/$PATTERN3/\1/g" "$1"`
 
 # Backup existing file
 mv -f "$1" "$1.bak"
 
 # Rewrite ruleset completely
 ./bin/dsort "$TEMP1" | uniq > "$TEMP2"
-./bin/xhttpse --num-threads 16 -o "$1" "$TEMP2"
+./bin/xhttpse --num-threads 16 --name "$NAME" -o "$1" "$TEMP2"
 
 # Remove temporary files
 rm -rf "$TEMP1" "$TEMP2"
