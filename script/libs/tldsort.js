1e029a99d849bfa1404f801c9f91bde082bb8eba
Chan Chak Shing <ccspasu@gmail.com>
2020-05-01 11:41:18 +0800

    Add script to sort a file consists of a list of domains

---
+++ script/libs/tldsort.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * Sort domains alphabetically by their top-level-domains.
+ * Remove duplicates if they exist.
+ *
+ * @param {string[]} domains An array of domains to be sorted
+ * @returns {string[]} sorted domains
+ */
+function tldsort(domains) {
+  return domains
+    .map((domain) => domain.split(".").reverse().join("."))
+    .sort()
+    .map((domain) => domain.split(".").reverse().join("."))
+    .filter((domain, index, arr) => arr.indexOf(domain, index + 1) < 0);
+}
+
+module.exports = tldsort;
